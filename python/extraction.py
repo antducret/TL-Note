3daaf15256549fb9cd96f972d7bf5ba4caaa9c9a
antducret <anthony.ducret@lilo.org>
2021-10-30 21:03:38 +0200

    Clarify extraction.py

--- python/extraction.py
+++ python/extraction.py
@@ -19,6 +19,7 @@ def extract_data(path):
     txt = pdf2text(path)
     data = dict()
 
+    # Date information
     date = re.search('\d{2}/\d{2}/\d{4}', txt)[0]
     date_tab = date.split("/")
     data["DAY"] =  int(date_tab[0])
@@ -26,50 +27,62 @@ def extract_data(path):
     data["YEAR"] = int(date_tab[2])
     data["WEEKDAY"] = weekday(data["DAY"],data["MONTH"],data["YEAR"])
 
+    # General info
     data["AGENT"] = re.search('\(\d{4}\)', txt)[0][1:-1]
     data["TURN"] = re.search('\d{5}', txt)[0]
     tmp_type = re.search('jrn.\s.*\s', txt)[0]
     tmp_type = re.sub("jrn.\s","",tmp_type)
     data["TYPE"] = re.search('Voiture|Radio|Form', txt)[0]
 
+    # --- SPECIFIC DATA -----
+
+    # Voiture
     CAR_tmp = re.search('Voiture\s+((\d+ +\- +\d+\s+)|Radio|Form|TD)+', txt)
-    #if CAR_tmp == None and db.debug: db.warn("CAR_tmp = None")
     CAR_tmp = CAR_tmp[0][9:].split("\n") if CAR_tmp != None else ["00000"]
-    data["CAR"] = [x for x in CAR_tmp if x]# List voiture
+    data["CAR"] = [x for x in CAR_tmp if x]
 
+    # Prise début
     P_I_tmp = re.search('Prise\sdébut\s(\d{2}:\d{2}\n)+\s', txt)
-    #if P_I_tmp == None and db.debug: db.warn("P_I_tmp = None")
     P_I_tmp = P_I_tmp[0][12:].split("\n") if P_I_tmp != None else ["00000"]
-    data["P_I"] = [x for x in P_I_tmp if x]# Début prise
+    data["P_I"] = [x for x in P_I_tmp if x]
 
+    # Lieu début
     L_I_tmp = re.search('Lieu\s(début)+\s(.+\n)+\s', txt)
-    #if L_I_tmp == None and db.debug: db.warn("L_I_tmp = None")
     L_I_tmp = L_I_tmp[0][11:].split("\n") if L_I_tmp != None else ["00000"]
+
+        #FIX (bug with mixed L_I, H_I)
     L_I_H_I_tmp = [x for x in L_I_tmp if x and "début" not in x]
     L_I_tmp = [re.search("[A-Z0-9\.]+",x)[0] for x in L_I_H_I_tmp if re.search("[A-Z]+",x)]
-    data["L_I"] = [x for x in L_I_tmp if x]# Lieu début
 
+    data["L_I"] = [x for x in L_I_tmp if x]
+
+    # Tour début
     H_I_tmp = re.search('Heure\sdébut\s(\d{2}:\d{2}\n)+\s', txt)
-    #if H_I_tmp == None and db.debug: db.warn("H_I_tmp = None")
     H_I_tmp = H_I_tmp[0][11:].split("\n") if H_I_tmp != None else [re.search("\d{2}:\d{2}",x)[0] for x in L_I_H_I_tmp if re.search("\d{2}:\d{2}",x)]
-    data["H_I"] = [x for x in H_I_tmp if x] # Début tour
 
+    data["H_I"] = [x for x in H_I_tmp if x]
+
+    # Tour fin
     H_F_tmp = re.search('Heure\sfin\s(\d{2}:\d{2}\n)+\s', txt)
-    #if H_F_tmp == None and db.debug : db.warn("H_F_tmp = None")
     H_F_tmp = H_F_tmp[0][9:].split("\n") if H_F_tmp != None else ["00000"]
-    data["H_F"] = [x for x in H_F_tmp if x]# Fin tour
 
+    data["H_F"] = [x for x in H_F_tmp if x]
+
+    # Lieu fin
     L_F_tmp = re.search('Lieu\sfin\s(.+\n)+\s', txt)
-    #if L_F_tmp == None and db.debug : db.warn("L_F_tmp = None")
     L_F_tmp = L_F_tmp[0][9:].split("\n") if L_F_tmp != None else ["00000"]
+
+        #FIX (bug with mixed L_F, H_I)
     L_F_H_F_tmp = [x for x in L_F_tmp if "fin" not in x]
     L_F_tmp = [re.search("[A-Z0-9\.]+",x)[0] for x in L_F_H_F_tmp if re.search("[A-Z]+",x)]
-    data["L_F"] = [x for x in L_F_tmp if x]# Lieu fin
 
+    data["L_F"] = [x for x in L_F_tmp if x]
+
+    # Prise fin ----
     P_F_tmp = re.search('Prise\sfin\s(\d{2}:\d{2}\n)+\s', txt)
-    #if P_F_tmp == None and db.debug : db.warn("P_F_tmp = None")
     P_F_tmp = P_F_tmp[0][10:].split("\n") if P_F_tmp != None else [re.search("\d{2}:\d{2}",x)[0] for x in L_F_H_F_tmp if re.search("\d{2}:\d{2}",x)]
-    data["P_F"] = [x for x in P_F_tmp if x] # Fin prise
+
+    data["P_F"] = [x for x in P_F_tmp if x]
 
     # Further details to determine
 
