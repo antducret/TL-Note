09c28d033c28c72f29f5cda34865cb00f8b0dede
antducret <anthony.ducret@lilo.org>
2021-10-31 12:44:26 +0100

    - change upload method from note to dict_note

--- python/moresimplenote.py
+++ python/moresimplenote.py
@@ -8,7 +8,8 @@ import datetime as dt
 def add_break(d,m,y,type,config_data,id_SN):
     """ upload a note for breakday on simple note"""
     note = cn.construct_break(d,m,y,type,config_data)
-    if cf.UPLOAD : id_SN.add_note(note)
+    date = dt.datetime(y,m,d)
+    upload_note(note, date, id_SN)
 
 def add_holiday(d0,m0,y0,d1,m1,y1,config_data,id_SN):
     """ upload a note for holiday on simple note"""
@@ -17,18 +18,29 @@ def add_holiday(d0,m0,y0,d1,m1,y1,config_data,id_SN):
     if date0 > date1:
         return 0
     else :
+        numdays = (date1-date0).days
+        dates = [date0 + datetime.timedelta(days=x) for x in range(numdays)]
         notes = cn.construct_holidays(d0,m0,y0,d1,m1,y1,config_data)
-        for note in notes :
-            if cf.UPLOAD : id_SN.add_note(note)
+        for i in range(len(notes)) :
+            upload_note(notes[i],dates[i],id_SN)
         return 1
-
 
 def add_agentcard(folder,config_data,id_SN,DEBUG = False):
     """ upload a note for work days on simple note"""
     pathfiles = sorted([f for f in os.listdir(folder) if f.endswith('.pdf')])
     for path in pathfiles :
-        note = cn.construct_card(folder+path,config_data,debug = DEBUG )
-        if cf.UPLOAD : id_SN.add_note(note)
+        note,date = cn.construct_card(folder+path,config_data,debug = DEBUG )
+        upload_note(note,date,id_SN)
+
+def upload_note(note,date,id_SN):
+    if cf.UPLOAD :
+        dict_note["key"] = "DATE"
+        dict_note["content"] = "NOTE"
+        dict_note["tags"] = "TAGS"
+        id_SN.add_note(dict_note)
+    else :
+        pass
+
 
 def outdate():
     """ WIP:  delete all outdated notes """
